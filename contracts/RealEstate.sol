aaf6c7459ab2da3d67b098d8c39615bd3506b515
Hp <allanokoth695@gmail.com>
2022-01-18 19:35:30 +0300

    add all

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -20,10 +20,10 @@ contract RealEstate {
   uint public propertyCounter;
 
   //constructor to initialize admin and Land Counter
-  constructor RealEstate () {
-    admin = msg.sender;
-    idCounter = 0;
-  }
+     constructor() public {
+         admin = msg.sender;
+        propertyCounter = 0;
+    }
 
   //modifier to verify sender is admin
   modifier isAdmin() {
@@ -32,43 +32,43 @@ contract RealEstate {
   }
 
   //event for Property Addition
-  event Add(address _owner, uint memory _id);
+  event Add(address _owner, uint _id);
 
   //event for Property transfer
   event Transfer(address indexed _from, address indexed _to, uint _land);
 
   //Function for adding/registering new land Ownership | Can only be executed by Contract Admin
-  function addProperty(string memory _location, uint memory _cost) public isAdmin{
+  function addProperty(string memory _location, uint _cost) public isAdmin{
     //Increase Property Counter by 1
     propertyCounter++;
     //Save New Property
     properties[propertyCounter] = Property(propertyCounter,msg.sender,_location,_cost);
 
     //Notify about Property Creation
-    Add(msg.sender, propertyCounter);
+    emit Add(msg.sender, propertyCounter);
   }
 
   //Function for transferring Property
-  function transferProperty(address memory _buyer, uint memory _id) public returns (bool) {
+  function transferProperty(address _buyer, uint _id) public returns (bool) {
     //Validate Sender is Property Owner
-    requires(msg.sender == properties[_id].owner);
+    require(msg.sender == properties[_id].owner);
 
     //Change Property Ownership
     properties[_id].owner == _buyer;
 
     //Notify transfer of Property Ownership
-    Transfer(msg.sender, _buyer, _id);
+    emit Transfer(msg.sender, _buyer, _id);
 
     return true;
   }
 
-  function fetchProperty(uint memory _id) public view returns (uint, address, string memory, uint) {
+  function fetchProperty(uint _id) public view returns (uint, address, string memory, uint) {
     return (
       properties[_id].id,
       properties[_id].owner,
       properties[_id].location,
-      properties[_id].cost,
-    )
+      properties[_id].cost
+    );
   }
 
 
